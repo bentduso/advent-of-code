aecc5406d2be81af65a0a4ff9b69604d64a2daab
Benjamin Zubača <bzubaca123@gmail.com>
2024-11-22 21:48:42 +0100

    Initial commit

---
+++ lib/advent_of_code/Y2024/day_01.ex
@@ -0,0 +1,5 @@
+defmodule AdventOfCode.Y2024.Day01 do
+  def hello() do
+    "Hello, Joe!"
+  end
+end
---
+++ test/advent_of_code/Y2024/day_01_test.exs
@@ -0,0 +1,9 @@
+defmodule AdventOfCode.Y2024.Day01Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Y2024.Day01
+
+  test "hello" do
+    assert hello() == "Hello, Joe!"
+  end
+end
