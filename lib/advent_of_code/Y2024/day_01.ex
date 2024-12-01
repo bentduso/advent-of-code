7315df82f4b0c822eb7b3a95e112df2e84121502
Benjamin Zubača <bzubaca123@gmail.com>
2024-12-01 12:46:57 +0100

    Refactor day 1 of 2024

--- lib/advent_of_code/Y2024/day_01.ex
+++ lib/advent_of_code/Y2024/day_01.ex
@@ -7,31 +7,21 @@ defmodule AdventOfCode.Y2024.Day01 do
     |> calculate_total_distance()
   end
 
-  defp parse_input(input) do
+  def parse_input(input) do
     input
     |> Input.as_lines()
-    |> Enum.map(&parse_line/1)
+    |> Enum.map(fn line ->
+      [left, right] = String.split(line, ~r{\s+})
+      {String.to_integer(left), String.to_integer(right)}
+    end)
+    |> Enum.unzip()
   end
 
-  defp parse_line(line) do
-    line
-    |> String.split(~r{\s+})
-    |> Enum.map(&String.to_integer/1)
-  end
-
-  defp calculate_total_distance(lines) do
-    left_sorted =
-      lines
-      |> Enum.map(&Enum.at(&1, 0))
-      |> Enum.sort()
-
-    right_sorted =
-      lines
-      |> Enum.map(&Enum.at(&1, 1))
-      |> Enum.sort()
-
-    Enum.zip(left_sorted, right_sorted)
-    |> Enum.map(fn {left, right} -> abs(left - right) end)
+  defp calculate_total_distance({left, right}) do
+    left
+    |> Enum.sort()
+    |> Enum.zip(Enum.sort(right))
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
     |> Enum.sum()
   end
 
@@ -41,16 +31,9 @@ defmodule AdventOfCode.Y2024.Day01 do
     |> calculate_similarity_score()
   end
 
-  defp calculate_similarity_score(lines) do
-    right_frequencies =
-      lines
-      |> Enum.map(&Enum.at(&1, 1))
-      |> Enum.frequencies()
-
-    left_values = Enum.map(lines, &Enum.at(&1, 0))
-
-    Enum.reduce(left_values, 0, fn number, acc ->
-      acc + number * Map.get(right_frequencies, number, 0)
+  defp calculate_similarity_score({left, right}) do
+    Enum.reduce(left, 0, fn number, acc ->
+      acc + number * Enum.count(right, &(&1 == number))
     end)
   end
 end
